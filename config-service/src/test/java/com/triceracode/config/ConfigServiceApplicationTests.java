4882107be00e78e7bd45b19ea665eadae55d1a14
AureoGonz <aureogonz@hotmail.com>
2020-09-03 12:23:33 -0500

    config-service

---
+++ config-service/src/test/java/com/triceracode/config/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.triceracode.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
