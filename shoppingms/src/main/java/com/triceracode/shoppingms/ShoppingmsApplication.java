310629e7548a6e5ca6d1712c5dc78e216c587eb8
AureoGonz <aureogonz@hotmail.com>
2020-09-03 12:59:12 -0500

    Eureka service (Server and Clients)

--- shoppingms/src/main/java/com/triceracode/shoppingms/ShoppingmsApplication.java
+++ shoppingms/src/main/java/com/triceracode/shoppingms/ShoppingmsApplication.java
@@ -2,8 +2,10 @@ package com.triceracode.shoppingms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ShoppingmsApplication {
 
 	public static void main(String[] args) {
